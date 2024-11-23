8a05a00f2b2140e30ee8f756d1075b40218b92cb
SayYeon <ko601008@naver.com>
2024-11-23 16:54:07 +0900

    커맨드 입력
    랜덤키 5개가 주어지고 올바른 키가 입력되면 sprite 꺼지는 것만 구현됨

---
+++ Assets/Scripts/CommandInput.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Direction
+{
+    Up, Down, Left, Right
+}
+
+public class CommandGame : MonoBehaviour
+{
+    [SerializeField] private Image[] directionImage;
+    [SerializeField] private Sprite[] directionSprites;
+    
+
+    private Stack<Image> imageStack = new Stack<Image>();
+    private Stack<Direction> directionStack = new Stack<Direction>();
+
+    void Start()
+    {
+        GenerateDirection();
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if ((Input.GetKeyDown(KeyCode.UpArrow) && directionStack.Peek() == Direction.Up) ||
+                (Input.GetKeyDown(KeyCode.DownArrow) && directionStack.Peek() == Direction.Down) ||
+                (Input.GetKeyDown(KeyCode.LeftArrow) && directionStack.Peek() == Direction.Left) ||
+                (Input.GetKeyDown(KeyCode.RightArrow) && directionStack.Peek() == Direction.Right))
+            {
+                CorrectInput();
+            }
+            else
+                IncorrectInput();
+        }
+    }
+
+    void GenerateDirection()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Direction currentDirection;
+            currentDirection = (Direction)Random.Range(0, 4);
+            int index = (int)currentDirection;
+            directionImage[i].sprite = directionSprites[index];
+            directionStack.Push(currentDirection);
+            imageStack.Push(directionImage[i]);
+        }
+    }
+
+    void CorrectInput()
+    {
+        directionStack.Pop();
+        imageStack.Pop().gameObject.SetActive(false);
+    }
+
+    void IncorrectInput()
+    {
+
+    }
+}
